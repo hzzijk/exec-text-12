5c909b8dd22aade307f31b669e92c639df202d6f
hzzijk <hzzijk@gmail.com>
2021-04-22 09:36:42 +0900

    123

---
+++ src/main/java/com/spboot/test/ExecTest12Application.java
@@ -0,0 +1,13 @@
+package com.spboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecTest12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecTest12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spboot/test/ExecTest12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spboot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecTest12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
